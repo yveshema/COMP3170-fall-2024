336af93ebb3db4c1bebd8f08e5fae1f6c7eaeefe
yves shema <yshema@bcit.ca>
2024-09-25 13:20:10 -0700

    event delegation

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,45 @@ function App() {
   }
 
   return (
+    <>
       <div className="outer" onClick={handleOuterClick}>
         <div className="inner" onClick={handleInnerClick}></div>
       </div>
+
+      <div className="container">
+        <Tile name="tile 1" />
+        <Tile name="tile 2" />
+        <Tile name="tile 3" />
+        <Tile name="tile 4" />
+        <Tile name="tile 5" />
+        <Tile name="tile 6" />
+      </div>
+    </>
+      
+  );
+}
+
+function Tile(props) {
+  const background = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+
+  function handleDelete(e) {
+    if ( e.target === e.currentTarget) {
+      // you clicked on the tile itself
+      return;
+    }
+
+    if (e.target.tagName !== 'SPAN') {
+      return;
+    }
+
+    e.currentTarget.remove(); // otherwise, remove the tile
+  }
+
+  return (
+    <div className="tile" style={{ backgroundColor: background }} onClick={handleDelete}>
+      <p>{props.name}</p>
+      <span>x</span>
+    </div>
   );
 }
 
